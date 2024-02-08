d8a90d7febd34f3090e6f3eb8e807f63d1a57142
brandonbande <89384636+brandonbande@users.noreply.github.com>
2024-02-08 14:02:24 +0200

    Fixed Onclick and added a few notes

--- main.js
+++ main.js
@@ -1,50 +1,43 @@
-const convertCurrency =() => {
-                const amountInput = document.getElementById("amount");
-                const fromCurrencySelect = document.getElementById("from");
-                const toCurrencySelect = document.getElementById("to");
-                const convertedAmountDisplay = document.getElementById("convertedamount");
-                const convertBtn = document.querySelector("convertBtn");
-
+//Variables
+const amountInput = document.getElementById("amount");
+const fromCurrencySelect = document.getElementById("from");
+const toCurrencySelect = document.getElementById("to");
+const convertedAmountDisplay = document.getElementById("convertedamount");
+const convertBtn = document.getElementById("convertBtn");
 
 const amount = parseFloat(amountInput.value);
-const fromCurrency= fromCurrencySelect.value;
+const fromCurrency = fromCurrencySelect.value;
 const toCurrency = toCurrencySelect.value;
 
+//Event Listener
+convertBtn.addEventListener("click", () => {
+  //ConvertingMoney(convertedAmountDisplay.value);
+  console.log("Clicked");
+});
 
-const host = 'api.frankfurter.app';
-fetch(`https://${host}/currencies`)
-.then((data) =>data.json())
-.then((data)=>{
-        const entries = Object.entries(data);
-        console.log(entries)
-}
-)
+//Function 1
+ async function ConvertingMoney() {
+   fetch(`https://open.er-api.com/v6/latest/usd`)
+     .then((response) => response.json())
+     .then((data) => {
+       const conversionRate = data.rates[toCurrency];
+       const convertedAmount = amount * conversionRate;
+       convertedAmountDisplay.textContent = convertedAmount.toFixed(2);
+       resultElement.innerHTML = `${convertedAmount} ${toCurrency}`;
+     });
+ }
 
+ //Function 2
+const convertCurrency = () => {
+  
+  const host = "api.frankfurter.app";
+  fetch(`https://${host}/currencies`)
+    .then((data) => data.json())
+    .then((data) => {
+      const entries = Object.entries(data);
+      console.log(entries);
+    });
+ 
+};
 
-
-
-
-async function ConvertingMoney () {
-     fetch(`https://open.er-api.com/v6/latest/usd`)
-     .then (response => response.json())
-     .then(data => {
-             const conversionRate = data.rates[toCurrency];
-             const convertedAmount = amount*conversionRate;
-             convertedAmountDisplay.textContent = convertedAmount.toFixed(2);
-             resultElement.innerHTML =`${convertedAmount} ${toCurrency}`
-     
-     })
-}
-
-
-
-
-convertBtn.addEventListener ("click" , () =>{
-          ConvertingMoney(convertedAmountDisplay.value); 
-
-}) 
-}
-
-
-
-
+convertCurrency();
