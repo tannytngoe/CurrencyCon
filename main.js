61d11252b16abccb991f50cf9364991f0b8a3b78
tannytngoe <147334337+tannytngoe@users.noreply.github.com>
2024-02-13 06:50:09 +0200

    added features

--- main.js
+++ main.js
@@ -1,51 +1,37 @@
 //Variables 
-const amountInput = document.getElementById("amount");
-const convertedAmountDisplay = document.getElementById("convertedamount");
-const convertBtn = document.querySelector("convertBtn");
+
 var select = document.querySelectorAll("from");
-var select = document .querySelectorAll('to');
+var select = document .querySelectorAll("to");
+amount = document.getElementById("amount");
+convertedamount = document.getElementById("convertedamount");
 
 
 
-const amount = parseFloat(amountInput.value);
-const fromCurrency = fromCurrencySelect.value;
-const toCurrency = toCurrencySelect.value;
 
-//Event Listener 
-convertBtn.addEventListener ("click" , () =>{
-      // ConvertingMoney(convertedAmountDisplay.value);
-       console.log("Clicked");
+//Funtion 1
 
-});
-//Function 1
-
-async function ConvertingMoney () {
-        fetch(`https://open.er-api.com/v6/latest/usd`)
-        .then (response => response.json())
-        .then(data => {
-                const conversionRate = data.rates[toCurrency];
-                const convertedAmount = amount*conversionRate;
-                convertedAmountDisplay.textContent = convertedAmount.toFixed(2);
-                resultElement.innerHTML =`${convertedAmount} ${toCurrency}`;
-        
-        });
-}
-
-//Funtion 2 
-const convertCurrency = () =>{ 
 
         const host = 'api.frankfurter.app';
         fetch(`https://${host}/currencies`)
           .then((data) => data.json())
           .then((data) => {
                 const entries = Object.entries(data);
-                console.log(entries)
+            console.log(entries)
             //alert(`10 GBP = ${data.rates.USD} USD`);
-          });
 
-};
+            for (i =0; i < entries.length; i++){
+                select[0].innerHTML += `<option value = "${entries[i][0]}">${entries[i][0]}</option>`;
+                select[1].innerHTML +=  `<option value = "${entries[i][0]}">${entries[i][0]}</option>`;
+               
+         }
+         });
 
-convertCurrency();
+            
+
+
+     
+
+
 
 
 
