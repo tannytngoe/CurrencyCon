eea4ac5921fa43116e30daf682532fcf72676de8
tannytngoe <147334337+tannytngoe@users.noreply.github.com>
2024-02-14 04:10:22 +0200

    added features

--- main.js
+++ main.js
@@ -1,32 +1,61 @@
-//Variables 
-
-var select = document.querySelectorAll("from");
-var select = document .querySelectorAll("to");
-amount = document.getElementById("amount");
-convertedamount = document.getElementById("convertedamount");
+//Currencies list 
+const currency_list = ["USD" ,"AUD", "GBP" ,"BGN" ,"BRL","CAD" ,"CHF" ,"CNY" ,"CZK" ,"DKK" ,"EUR" ,"GBP","HKD","HUF" ,"IDR","ILS","INR" ,"ISK" ,"JPY" ,"KRW" ,"MXN","NOK","NZD"  ,"PHP" ,"PLN", "RON" ,"SEK" ,"SGD" ,"THB" ,"TRY" ,"ZAR"];
 
 
+//Variables
+
+        const  fromCurrencySelect = document.getElementById("from");
+        const toCurrencySelect= document .getElementById("to");
+        const amountInput = document.getElementById("amount");
+        const convertedAmountInput = document.getElementById("convertedamount");
+        const convertBtn = document.getElementById("convertBtn")
+
+const amount = parseFloat(amountInput.value);
+const fromCurrency = fromCurrencySelect.value;
+const toCurrency = toCurrencySelect.value;
+
+//Event Listener 
+convertBtn.addEventListener("click ",() => {
+       //ConvertingMoney(convertedAmountDisplay.value);
+       console.log("Clicked") ;
+
+});
+
+//Function 1 
+
+async function ConvertingMoney(){
+    fetch(`https://open.er-api.com/v6/latest/usd`)
+    .then ((response) => response.json ())
+    .then((data)  =>{
+          const conversionRate = data.rates [toCurrency];
+          const convertedAmount = amount * conversionRate;
+          convertedAmountDisplay.textContent = convertedAmount.toFixed(2);
+          resultElement.innerHTML = `${convertedAmount} ${toCurrency}`;
+
+    });
 
 
-//Funtion 1
+    }
 
 
-        const host = 'api.frankfurter.app';
-        fetch(`https://${host}/currencies`)
-          .then((data) => data.json())
-          .then((data) => {
-                const entries = Object.entries(data);
-            console.log(entries)
-            //alert(`10 GBP = ${data.rates.USD} USD`);
+//loop thru cuurency list 
+//create an option
+//set value and teext to currency 
+currency_list.forEach((currency)    =>{
+            const option = document.createElement ("option");
+            option.value = currency;
+            option.text = currency;
+            fromCurrencySelect.appendChild(option);
 
-            for (i =0; i < entries.length; i++){
-                select[0].innerHTML += `<option value = "${entries[i][0]}">${entries[i][0]}</option>`;
-                select[1].innerHTML +=  `<option value = "${entries[i][0]}">${entries[i][0]}</option>`;
-               
-         }
-         });
+            if (currency !== fromCurrencySelect.value){
+              toCurrencySelect.appendChild(option.cloneNode(true));
+            }
+
+});
 
             
+
+
 
 
      
